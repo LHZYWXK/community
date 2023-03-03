9958781798446e278237de2e27aa217770fbaff8
luhaozheng <haozhenglu@foxmail.com>
2023-03-03 18:35:50 +0800

    Show login message

---
+++ src/main/java/hk/hku/cs/community/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package hk.hku.cs.community.config;
+
+import hk.hku.cs.community.controller.interceptor.LoginTicketInterceptor;
+import hk.hku.cs.community.entity.LoginTicket;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Autowired
+    private LoginTicketInterceptor loginTicketInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginTicketInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+    }
+}
---
+++ src/main/java/hk/hku/cs/community/utils/HostHolder.java
@@ -0,0 +1,26 @@
+package hk.hku.cs.community.utils;
+
+import hk.hku.cs.community.entity.User;
+import org.springframework.stereotype.Component;
+
+/**
+ * 持有用户的信息，用于代替 session 对象
+ * 线程隔离的
+ */
+@Component
+public class HostHolder {
+    private ThreadLocal<User> users = new ThreadLocal<>();
+
+    public void setUser(User user) {
+        users.set(user);
+    }
+
+    public User getUser() {
+        return users.get();
+    }
+
+    public void clear() {
+        users.remove();
+    }
+
+}
