6256edac49f719e995490cb8ba0978c299fc1ef6
luhaozheng <haozhenglu@foxmail.com>
2023-03-10 14:51:59 +0800

    Add comment related function

---
+++ src/main/java/hk/hku/cs/community/controller/DiscussPostController.java
@@ -0,0 +1,118 @@
+package hk.hku.cs.community.controller;
+
+import hk.hku.cs.community.entity.Comment;
+import hk.hku.cs.community.entity.DiscussPost;
+import hk.hku.cs.community.entity.Page;
+import hk.hku.cs.community.entity.User;
+import hk.hku.cs.community.service.CommentService;
+import hk.hku.cs.community.service.DiscussPostService;
+import hk.hku.cs.community.service.UserService;
+import hk.hku.cs.community.utils.CommunityConstant;
+import hk.hku.cs.community.utils.CommunityUtils;
+import hk.hku.cs.community.utils.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.*;
+
+@Controller
+@RequestMapping("/discuss")
+public class DiscussPostController implements CommunityConstant {
+    @Autowired
+    private DiscussPostService discussPostService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private CommentService commentService;
+
+    @RequestMapping(path = "/add", method = RequestMethod.POST)
+    @ResponseBody
+    public String addDiscussPost(String title, String content) {
+        User user = hostHolder.getUser();
+        if (user == null) {
+            return CommunityUtils.getJSONString(403, "您还没有登陆！");
+        }
+
+        DiscussPost discussPost = new DiscussPost();
+        discussPost.setUserId(user.getId());
+        discussPost.setTitle(title);
+        discussPost.setContent(content);
+        discussPost.setCreateTime(new Date());
+
+        discussPostService.addDiscussPost(discussPost);
+
+        return CommunityUtils.getJSONString(0, "发布成功！");
+    }
+
+    @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model, Page page) {
+        DiscussPost discussPost = discussPostService.findDiscussPostById(discussPostId);
+        model.addAttribute("post", discussPost);
+
+        User user = userService.findUserById(discussPost.getUserId());
+        model.addAttribute("user", user);
+
+        page.setLimit(5);
+        page.setPath("/discuss/detail/" + discussPostId);
+        page.setRows(discussPost.getCommentCount());
+
+        // 评论：给帖子的评论
+        // 回复：给评论的评论
+        // commentList：评论列表
+        List<Comment> commentList = commentService.findCommentsByEntity(
+                ENTITY_TYPE_POST, discussPost.getId(), page.getOffset(), page.getLimit()
+        );
+
+        // commentVoList：评论 View Object 列表
+        List<Map<String, Object>> commentVoList = new ArrayList<>();
+        if (commentList != null) {
+            for (Comment comment : commentList) {
+                // commentVo：评论的 VO
+                Map<String, Object> commentVo = new HashMap<>();
+                // 评论
+                commentVo.put("comment", comment);
+                // 作者
+                commentVo.put("user", userService.findUserById(comment.getUserId()));
+                // 回复列表
+                List<Comment> replyList = commentService.findCommentsByEntity(
+                        ENTITY_TYPE_COMMENT, comment.getId(), 0, Integer.MAX_VALUE
+                );
+                // 回复 VO 列表
+                List<Map<String, Object>> replyVoList = new ArrayList<>();
+                if (replyList != null) {
+                    for (Comment reply : replyList) {
+                        Map<String, Object> replyVo = new HashMap<>();
+                        // 回复
+                        replyVo.put("reply", reply);
+                        // 作者
+                        replyVo.put("user", userService.findUserById(reply.getUserId()));
+                        // 回复的目标
+                        User target = reply.getTargetId() == 0 ? null : userService.findUserById(reply.getTargetId());
+                        replyVo.put("target", target);
+                        replyVoList.add(replyVo);
+                    }
+                }
+                commentVo.put("replies", replyVoList);
+                // 回复数量
+                int replyCount = commentService.findCommentCount(ENTITY_TYPE_COMMENT, comment.getId());
+                commentVo.put("replyCount", replyCount);
+                commentVoList.add(commentVo);
+            }
+        }
+
+        model.addAttribute("comments", commentVoList);
+
+        return "/site/discuss-detail";
+    }
+
+}
---
+++ src/main/java/hk/hku/cs/community/dao/CommentMapper.java
@@ -0,0 +1,17 @@
+package hk.hku.cs.community.dao;
+
+import hk.hku.cs.community.entity.Comment;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CommentMapper {
+
+    List<Comment> selectCommentsByEntity(int entityType, int entityId, int offset, int limit);
+
+    int selectCountByEntity(int entityType, int entityId);
+
+    int insertComment(Comment comment);
+
+}
